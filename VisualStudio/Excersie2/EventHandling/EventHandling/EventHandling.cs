b27dc98df9669ff37e03842f04c6d7b306bad2df
Tim Mend <tim.mend@uni-ulm.de>
2017-04-30 02:50:37 +0200

    Started with Excersice 2
    I started with excersice but i didn#t debug it...

---
+++ VisualStudio/Excersie2/EventHandling/EventHandling/EventHandling.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EventHandling
+{
+    public class UserController
+    {
+        //Initiating the userList List
+        List<UserModel> userList;
+
+
+        //Constructor
+        public UserController()
+        {
+            this.userList = new List<UserModel>();
+
+        }
+
+
+        /*
+         * This is the struct which is needed 
+         */
+        struct UserModel
+        {
+            String Name;
+            String Vorname;
+            int Age;
+            public UserModel(String name, String vorname, int age)
+            {
+                this.Name = name;
+                this.Vorname = vorname;
+                this.Age = age;
+            }
+        }
+
+        //Enum for the States of the Programm
+        public enum State
+        {
+            Init, New, Edit
+        }
+
+        //Event UserEventArgs
+        public class UserEventArgs : EventArgs
+        {
+            public int id;
+        }
+
+        //Event ErrorEventArgs
+        public class ErrorEventArgs : EventArgs
+        {
+            public String Message;
+        }
+
+        //EventHandler for a UserEvent
+        public event EventHandler<UserEventArgs> NewUserEvent;
+        //EventHandler for a UserEvent
+        public event EventHandler<UserEventArgs> UserEditedEvent;
+        //EventHandler for a ErrorEvent
+        public event EventHandler<ErrorEventArgs> ErrorEvent;
+
+        /*
+         * Method NewUser to Add a User to the List. Before Adding the New User the Method
+         * will test if the input is correct
+         */
+        public void NewUser(String name)
+        {
+            String[] split = name.Split(',');
+            int age = 0;
+            if (split.Length != 3 || Int32.TryParse(split[2], out age))
+            {
+                //Generate new Error message
+                ErrorEventArgs myArgs = new ErrorEventArgs() { Message = "Your input was wrong, it should have the format 'name, vorname, age" };
+                ErrorEvent?.Invoke(this, myArgs);
+                //For testing
+                //Console.WriteLine("Something went wrong, try again!");
+            }
+
+            userList.Add(new UserModel(split[0], split[1], age));
+            //Genrate Event
+            UserEventArgs myArgs1 = new UserEventArgs() { id = userList.Count - 1 };
+            NewUserEvent?.Invoke(this, myArgs1);
+        }
+
+
+
+        /*
+         *Method EditUser to Edit a User in the List. Before Inserting a new User at specific
+         * point the Method will check if the Input String is correct
+         */
+        public void EditUser(String name)
+        {
+            String[] split = name.Split(',');
+            int age = 0;
+            int id = 0;
+            if (split.Length != 4 || !Int32.TryParse(split[0], out id) || !Int32.TryParse(split[3], out age))
+            {
+                //Generate new Error message
+                ErrorEventArgs myArgs = new ErrorEventArgs() { Message = "Your input was wrong, it should have the format 'id, name, vorname, age" };
+                ErrorEvent?.Invoke(this, myArgs);
+                //Console.WriteLine("Something went wrong, try again!");
+            }
+            userList.Insert(id, new UserModel(split[1], split[2], age));
+            //Generate Event
+            UserEventArgs myArgs1 = new UserEventArgs() { id = userList.Count - 1 };
+            UserEditedEvent?.Invoke(this, myArgs1);
+        }
+
+
+    }
+}
---
+++ VisualStudio/Excersie2/EventHandling/EventHandling/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static EventHandling.UserController;
+
+namespace EventHandling
+{
+    class Program
+    {
+
+        private static void HandleNewUserEvent(object send, UserEventArgs args)
+        {
+            Console.WriteLine(args.id);
+        }
+
+        private static void HandleUserEditedEvent(object sender, UserEventArgs args)
+        {
+            Console.WriteLine(args.id);
+        }
+
+        private static void HandleErrorEvent(object sender, ErrorEventArgs args)
+        {
+            Console.WriteLine(args.Message);
+        }
+        //Initiating the userController Class
+        static UserController userController = new UserController();
+
+        /*
+         * Main Method where the Application will be started
+         */
+        static void Main(string[] args)
+        {
+            //Initiating the Enum
+            EventHandling.UserController.State state = UserController.State.Init;
+            //The loop which will get the input from the user
+            while (true)
+            {
+                String userInput = Console.ReadLine();
+                //if the user types exit the application will be terminated
+                if (userInput.Equals("exit"))
+                {
+                    Environment.Exit(0);
+                }
+                //if the user types new the state will be set to New
+                if (userInput.Equals("new"))
+                {
+                    state = UserController.State.New;
+                }
+                //if the user tpyes edit the state will be set to Edit
+                if (userInput.Equals("edit"))
+                {
+                    state = UserController.State.Edit;
+                }
+
+                //the switch which decides which method will be called
+                switch (state)
+                {
+                    //if the state is edit Edit User will be called
+                    case UserController.State.Edit:
+                        {
+                            userController.EditUser(userInput);
+                            userController.NewUserEvent += HandleUserEditedEvent;
+                            break;
+                        }
+                        //if the state is new New User will be called
+                    case UserController.State.New:
+                        {
+                            userController.NewUser(userInput);
+                            userController.NewUserEvent += HandleNewUserEvent;
+                            break;
+                        }
+
+                }
+            }
+        }
+    }
+}
