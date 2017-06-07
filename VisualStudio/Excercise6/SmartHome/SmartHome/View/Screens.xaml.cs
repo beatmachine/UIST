dfb32e20ddba749ec66a32142e4ac05560edd6c5
Tim Mend <tmend@gmx.net>
2017-06-07 14:56:22 +0200

    lil Push

---
+++ VisualStudio/Excercise6/SmartHome/SmartHome/View/Screens.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SmartHome.View
+{
+    /// <summary>
+    /// Interaktionslogik für Screens.xaml
+    /// </summary>
+    public partial class Screens : UserControl
+    {
+        public Screens()
+        {
+            InitializeComponent();
+        }
+
+        private void Livingroom_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Bath_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Workroom_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Kitchen_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
